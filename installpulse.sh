7f916ddd6083a7fc452080a4391ffca790dc80a7
Pravin Bhagade <pbhagade@cloudera.com>
2022-06-01 11:13:44 +0530

    Update installpulse.sh

--- installpulse.sh
+++ installpulse.sh
@@ -11,13 +11,17 @@
 # Number of Pulse Core Servers : 16
 # Pulse Server Storage : 1TB
 
-# set JAVA_HOME
+echo "Set JAVA_HOME"
 
 find / -executable -name java
 export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.332.b09-1.el7_9.x86_64
+echo $JAVA_HOME
+
+echo "Set umask"
+umask
 echo umask 0022 >> /etc/profile
 
-# sestatus
+echo "sestatus status" 
 
 sestatus
 setenforce 0
@@ -27,7 +31,7 @@ sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
 # DNS	Require DNS resolution from Pulse server to cluster nodes or from cluster nodes to Pulse server. Test with nslookup command to check forward and reverse lookup of hosts.
 cat /etc/resolv.conf
 
-# sysctl
+echo "Sysctl Status"
 
 sysctl -w vm.max_map_count=262144
 sysctl -w net.ipv4.ip_forward=1
@@ -35,7 +39,7 @@ sudo sh -c "echo 'vm.max_map_count=262144' >> /etc/sysctl.conf"
 sudo sh -c "echo 'net.ipv4.ip_forward=1' >> /etc/sysctl.conf"
 sudo sysctl -p /etc/sysctl.conf
 
-# Firewall
+echo "Firewall Status"
 
 sudo firewall-cmd --state
 firewall-cmd --get-default-zone
