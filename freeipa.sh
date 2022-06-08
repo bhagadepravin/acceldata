5451d93bb340059ecafa850dda322c1941a58ea0
Pravin Bhagade <pbhagade@cloudera.com>
2022-06-08 16:05:45 +0530

    Update freeipa.sh

--- freeipa.sh
+++ freeipa.sh
@@ -33,7 +33,7 @@ sudo sysctl -p /etc/sysctl.conf
 git clone https://github.com/freeipa/freeipa-container.git
 cd freeipa-container
 docker build -t freeipa-server -f Dockerfile.centos-7 .
-docker images
+docker images freeipa-server
 
 # docker run  -e IPA_SERVER_IP=<...ip...> --name freeipa-server -ti -h <HOSTNAME> -p 53:53/udp -p 53:53 -p 80:80 -p 443:443 -p 389:389 -p 636:636 -p 88:88 -p 464:464 -p 88:88/udp -p 464:464/udp -p 123:123/udp --sysctl net.ipv6.conf.all.disable_ipv6=0 -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v /var/lib/ipa-data:/data:Z -e PASSWORD=admin-password freeipa-server ipa-server-install -U -r <REALM> --ds-password=admin-password --admin-password=admin-password --domain=<DOMAIN> --no-ntp 
 
