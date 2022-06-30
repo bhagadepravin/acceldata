2ab5553191bf03d131272db45e842b055171da1c
Pravin Bhagade <iampravinbhagade@gmail.com>
2022-06-30 16:45:53 +0530

    Update torch.sh

--- torch.sh
+++ torch.sh
@@ -3,7 +3,7 @@
 # Company: Acceldata
 # Designation: Staff SRE
 
-# rm -rf torch.sh && wget https://raw.githubusercontent.com/bhagadepravin/acceldata/main/torch.sh && chmod +x torch.sh && ./torch.sh
+# rm -rf torch.sh && wget https://raw.githubusercontent.com/bhagadepravin/acceldata/full-torch/torch.sh && chmod +x torch.sh && ./torch.sh
 
 set -E
 
