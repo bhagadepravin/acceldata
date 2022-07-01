63364e4047aab9efb8f018e70575da24b7703dba
Pravin Bhagade <iampravinbhagade@gmail.com>
2022-07-01 11:40:59 +0530

    Update torch.sh

--- torch.sh
+++ torch.sh
@@ -153,7 +153,7 @@ function delete_torch {
     [ -e /usr/bin/docker ] && docker rm $(docker ps -a -q)
     [ -e /usr/bin/docker ] && docker system prune --force
     [ -e /usr/bin/docker ] && docker network prune --force
-    yum remove -q -y docker-ce docker containerd.io && rm -rf /var/lib/docker > /dev/null
+    yum remove -q -y docker-ce docker-ce-cli containerd.io && rm -rf /var/lib/docker > /dev/null
     yum remove -q -y kubeadm kubectl kubelet kubernetes-cni && rm -rf /usr/bin/kubeadm 
     [ -e /usr/local/bin/kubectl ] && rm -rf /usr/local/bin/kubectl
     [ -e /var/lib/kubelet ] && rm -rf /var/lib/kubelet
