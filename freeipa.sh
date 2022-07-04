f85b364d042fb274d5200285a8046751edd27794
Pravin Bhagade <iampravinbhagade@gmail.com>
2022-07-04 17:29:52 +0530

    Update freeipa.sh

--- freeipa.sh
+++ freeipa.sh
@@ -55,8 +55,8 @@ echo "${GREEN} Enable Port forwading${NC}
     echo "IP=${GREEN}${IP}${NC}"
     echo "DOMAIN=${GREEN}${REALM}${NC}"
     echo "REALM=${GREEN}${DOMAIN}${NC}"
-
-    docker run -e IPA_SERVER_IP=${IP} --name freeipa-server -ti -h ${HOSTNAME} \
+    
+docker run -e IPA_SERVER_IP=${IP} --name freeipa-server -ti -h ${HOSTNAME} \
         -p 53:53/udp -p 53:53 -p 80:80 -p 443:443 -p 389:389 -p 636:636 -p 88:88 -p 464:464 -p 88:88/udp -p 464:464/udp \
         --sysctl net.ipv6.conf.all.disable_ipv6=0 -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v /var/lib/ipa-data:/data:Z \
         -e PASSWORD=admin-password freeipa-server ipa-server-install -U -r ${REALM} --ds-password=admin-password --admin-password=admin-password \
