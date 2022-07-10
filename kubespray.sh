77f14b2b08f947e902689f38eee7c8a54437b882
Pravin Bhagade <iampravinbhagade@gmail.com>
2022-07-10 14:50:45 +0530

    Update kubespray.sh

--- kubespray.sh
+++ kubespray.sh
@@ -2,24 +2,31 @@
 
 # curl -sSL https://raw.githubusercontent.com/bhagadepravin/acceldata/main/kubespray.sh | bash
 
-yum -y install epel-release
-yum -y update
+yum -y install epel-release 2>/dev/null >/dev/null
+yum -y update  2>/dev/null >/dev/null
 yum -y groupinstall "Development Tools"
-yum -y install openssl-devel bzip2-devel libffi-devel xz-devel
-yum -y install wget
-wget https://www.python.org/ftp/python/3.8.12/Python-3.8.12.tgz
-tar xvf Python-3.8.12.tgz
-cd Python-3.8*/
-./configure --enable-optimizations
-sudo make altinstall
-python3.8 --version
-sudo yum -y install gcc gcc-c++ python-virtualenv python-pip python-devel libffi-devel openssl-devel libyaml-devel sshpass git vim-enhanced
+yum -y install openssl-devel bzip2-devel libffi-devel xz-devel wget mlocate git 2>/dev/null >/dev/null
+if [[ -f "/usr/local/bin/python3.8" ]]
+then
+    echo "python3.8 is present"
+else 
+    printf "Downloading Python-3.9.6 tar "
+    wget https://www.python.org/ftp/python/3.8.12/Python-3.8.12.tgz
+    tar xvf Python-3.8.12.tgz
+    cd Python-3.8*/
+    ./configure --enable-optimizations
+    sudo make altinstall
+    python3.8 --version
+    printf "done!\n"
+fi
+
+sudo yum -y install gcc gcc-c++ python-virtualenv python-pip python-devel libffi-devel openssl-devel libyaml-devel sshpass git vim-enhanced 2>/dev/null >/dev/null
 python3 get-pip.py
 pip3 install virtualenv
 pip3 install virtualenvwrapper
 cd
 #git clone https://github.com/kubernetes-sigs/kubespray.git
-git clone  https://github.com/bhagadepravin/kubespray.git
+git clone  https://github.com/bhagadepravin/kubespray.git 2>/dev/null >/dev/null
 HOSTNAME=`hostname -f`
 IPADDRESS=$(ip route get 8.8.8.8 | awk -F"src " 'NR==1{split($2,a," ");print a[1]}')
 sed -i "s/IPADDRESS/${IPADDRESS}/g" kubespray/inventory/pravincluster/inventory.ini 2>/dev/null >/dev/null
