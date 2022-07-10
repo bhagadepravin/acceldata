3c68bd0b0ce87c32496680c0142a18919ab57959
Pravin Bhagade <iampravinbhagade@gmail.com>
2022-07-10 13:41:01 +0530

    Update freeipa.sh

--- freeipa.sh
+++ freeipa.sh
@@ -61,10 +61,15 @@ echo "${GREEN} Enable Port forwading${NC}
     echo "REALM=${GREEN}${REALM}${NC}"
 
 echo "run below cmd\n"    
+docker run -e IPA_SERVER_IP=${IP} --name freeipa-server -ti -h ${HOSTNAME} \
+        -p 53:53/udp -p 53:53 -p 80:80 -p 443:443 -p 389:389 -p 636:636 -p 88:88 -p 464:464 -p 88:88/udp -p 464:464/udp \
+        --sysctl net.ipv6.conf.all.disable_ipv6=0 -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v /var/lib/ipa-data:/data:Z \
+        -e PASSWORD=admin-password pravinbhagade/freeipa-server ipa-server-install -U -r ${REALM} --ds-password=admin-password --admin-password=admin-password \
+        --domain=${DOMAIN} --no-ntp
 echo "docker run -e IPA_SERVER_IP=${IP} --name freeipa-server -ti -h ${HOSTNAME} \
         -p 53:53/udp -p 53:53 -p 80:80 -p 443:443 -p 389:389 -p 636:636 -p 88:88 -p 464:464 -p 88:88/udp -p 464:464/udp \
         --sysctl net.ipv6.conf.all.disable_ipv6=0 -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v /var/lib/ipa-data:/data:Z \
-        -e PASSWORD=admin-password freeipa-server ipa-server-install -U -r ${REALM} --ds-password=admin-password --admin-password=admin-password \
+        -e PASSWORD=admin-password pravinbhagade/freeipa-server ipa-server-install -U -r ${REALM} --ds-password=admin-password --admin-password=admin-password \
         --domain=${DOMAIN} --no-ntp"
 fi
 # docker stop freeipa-server
