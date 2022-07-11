e53e2ccd6946065e99b2edb67481b78684809419
Pravin Bhagade <iampravinbhagade@gmail.com>
2022-07-11 14:34:17 +0530

    Update installpulse.sh

--- installpulse.sh
+++ installpulse.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 # Pravin Bhagade
 
+set -E 
+GREEN=$'\e[0;32m'
+RED=$'\e[0;31m'
+NC=$'\e[0m'
+
 # wget https://raw.githubusercontent.com/bhagadepravin/acceldata/main/installpulse.sh && chmod +x installpulse.sh && bash installpulse.sh
 # curl -sSL https://raw.githubusercontent.com/bhagadepravin/acceldata/main/installpulse.sh | bash
 
@@ -15,58 +20,64 @@
 # Number of Pulse Core Servers : 16
 # Pulse Server Storage : 1TB
 
-echo "Set JAVA_HOME"
+echo "Check if JAVA_HOME exists or not"
+if [ -z "${JAVA_HOME}" ]
+then
+    JAVA_HOME=$(readlink -nf $(which java) | xargs dirname | xargs dirname | xargs dirname)
+    if [ ! -e "$JAVA_HOME" ]
+    then
+        JAVA_HOME=""
+    fi
+    export JAVA_HOME=$JAVA_HOME
+    echo "export JAVA_HOME=$JAVA_HOME" >> ~/.bash_profile
+fi
 
-find / -executable -name java
-echo $JAVA_HOME
+grep 022 /etc/profile 2>/dev/null >/dev/null
+if [ $? -eq 0 ]; then
+else
+   echo "umask 0022" >> /etc/profile
+   fi
 
-echo "Set umask"
-umask
-echo umask 0022 >> /etc/profile
-
-echo "sestatus status" 
-
-sestatus
+sestatus  | grep "SELinux status" | grep enabled 2>/dev/null >/dev/null
+if [ $? -eq 0 ]; then
 setenforce 0
-sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config 2>/dev/null >/dev/null
+else
+echo "SELinux is disabled already"
+fi
 
 
 # DNS	Require DNS resolution from Pulse server to cluster nodes or from cluster nodes to Pulse server. Test with nslookup command to check forward and reverse lookup of hosts.
-cat /etc/resolv.conf
-
-echo "Sysctl Status"
-
-sysctl -w vm.max_map_count=262144
-sysctl -w net.ipv4.ip_forward=1
-sudo sh -c "echo 'vm.max_map_count=262144' >> /etc/sysctl.conf"
-sudo sh -c "echo 'net.ipv4.ip_forward=1' >> /etc/sysctl.conf"
-sudo sysctl -p /etc/sysctl.conf
-
+# cat /etc/resolv.conf
+    echo "${GREEN} Enable Port forwading${NC}"
+sed -i "/enp0s3/d" /etc/sysctl.conf 2>/dev/null >/dev/null
+sysctl -w vm.max_map_count=262144  2>/dev/null >/dev/null
+sysctl -w net.ipv4.ip_forward=1 2>/dev/null >/dev/null
+sudo sh -c "echo 'vm.max_map_count=262144' >> /etc/sysctl.conf" 2>/dev/null >/dev/null
+sudo sh -c "echo 'net.ipv4.ip_forward=1' >> /etc/sysctl.conf" 2>/dev/null >/dev/null
+sudo sysctl -p /etc/sysctl.conf 2>/dev/null >/dev/null
+    
 echo "Firewall Status"
 
 sudo firewall-cmd --state
 firewall-cmd --get-default-zone
 firewall-cmd --get-active-zones
-systemctl status firewalld
-systemctl stop firewalld
+systemctl status firewalld && systemctl stop firewalld
 
 # Increase LVM size for root
-yum -y install cloud-utils-growpart && growpart /dev/sda 2; pvresize /dev/sda2; lvextend -l+100%FREE /dev/centos/root; xfs_growfs /dev/centos/root;lsblk
+# yum -y install cloud-utils-growpart && growpart /dev/sda 2; pvresize /dev/sda2; lvextend -l+100%FREE /dev/centos/root; xfs_growfs /dev/centos/root;lsblk
 
-# Docker without proxy setup
-
-###  Add Docker repository ###
-# Install dependencies for docker-ce
-sudo yum -y install yum-utils device-mapper-persistent-data lvm2
-
-echo "Added docker repo"
-yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo 
-
-yum clean all && yum update all  && yum install -y wget git vim docker-ce iptables docker-ce-cli containerd.io
-docker version
-
-# Setup daemon.
-cat > /etc/docker/daemon.json <<EOF
+which docker 2>/dev/null && docker --version | grep "Docker version" >/dev/null
+if [ $? -eq 0 ]; then
+    echo "${GREEN}Docker Existing${NC}"
+else
+    echo "${RED}Install Docker.......${NC}"
+    sudo yum -y install yum-utils device-mapper-persistent-data lvm2 2>/dev/null >/dev/null
+    yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo 2>/dev/null >/dev/null
+    yum clean all 2>/dev/null >/dev/null && yum update all 2>/dev/null >/dev/null
+    echo "${GREEN}Installing Docker Packages${NC}"
+    yum install -y wget git vim docker-ce iptables docker-ce-cli containerd.io 2>/dev/null >/dev/null
+    cat > /etc/docker/daemon.json <<EOF
 {
 "live-restore": true,
 "log-driver": "json-file",
@@ -78,16 +89,14 @@ cat > /etc/docker/daemon.json <<EOF
 }
 }
 EOF
-
-# Restart Docker
 systemctl daemon-reload
-systemctl enable docker
+systemctl enable docker 2>/dev/null >/dev/null
 systemctl restart docker
-systemctl status -l docker
+    docker version --format '{{.Server.Version}}'
+fi
 
-echo umask 0022 >> /etc/profile
-
-mkdir -p /acceldata
+ [ -e /acceldata ] && echo "/acceldata exists" && mv -f /acceldata /acceldata_bk || mkdir -p /acceldata
+ 
 
 # Cluster Configuration Changes
 # https://docs.acceldata.io/pulse/cluster-configuration-changes
